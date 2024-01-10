9075190560fa6103a3ab900391f5dd9810444b78
Yedigaryan <davit.yedigaryan.1993@gmail.com>
2024-01-11 03:41:48 +0400

    created frames component , refactored code

--- src/components/MainFrame.tsx
+++ src/components/MainFrame.tsx
@@ -4,6 +4,7 @@ import Numbers from "./Numbers";
 import {BlueFlame} from "./BlueFlame";
 import styles from "./MainFrame.module.scss"
 import {Chain} from "./Chain";
+import {Frames} from "./Frames";
 
 export default function MainFrame() {
     const [isSecondStage, setIsSecondStage] = useState(false);
@@ -19,109 +20,21 @@ export default function MainFrame() {
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.centerContent}>
-                    <motion.div
-                        // initial={{top: '0', left: '0', translate: "(-50%, -50%)"}}
-                        // animate={{
-                        //     rotateY: "0.5turn",
-                        // }}
-                        // transition={{
-                        //     duration: 10,
-                        //     ease: "backOut", when: "afterChildren", delay: 7
-                        // }}
-                        className={styles.frames}>
-                        <div>
-                            <div
-                                className={styles.topFrames}
-                            >
-                                <motion.div
-                                    className={styles.frame}
-                                    initial={{width: '0px', height: '0px'}}
-                                    animate={{
-                                        width: ['0px', '5px', '10px'],
-                                        height: ['0px', '5px', '10px']
-                                    }}
-                                    transition={{
-                                        duration: frameAppearDuration,
-                                        times: [0.1, 0.3, 0.5],
-                                        ease: "backOut"
-                                    }}
-                                />
-
-                                <motion.div
-                                    className={styles.frame}
-                                    initial={{height: '10px', width: '0px'}}
-                                    animate={{
-                                        width: ['0px', '5px', '10px'],
-                                        height: ['0px', '5px', '10px'],
-                                    }}
-                                    transition={{
-                                        duration: frameAppearDuration,
-                                        times: [0.1, 0.3, 0.5],
-                                        ease: "easeInOut"
-                                    }}
-                                />
-                            </div>
-
-                            <motion.div
-                                className={styles.bottomFrames}
-                                initial={{top: '5px', left: 0}}
-                                animate={{top: ['1vh', '1vh', '60vh']}}
-                                transition={{
-                                    duration: 1,
-                                    times: [0.1, 0.5, 1],
-                                    ease: "easeInOut",
-                                    delay: frameAppearDuration
-                                }}
-                            >
-                                <motion.div
-                                    className={styles.frame}
-                                    initial={{height: '0px', width: '10px', color: "black", top: '10px', left: 0}}
-                                    animate={{
-                                        height: ['0px', '5px', '10px'],
-                                        width: ['0px', '5px', '10px'],
-                                    }}
-                                    transition={{
-                                        duration: frameAppearDuration,
-                                        times: [0.1, 0.3, 0.5],
-                                        ease: "easeInOut"
-                                    }}
-                                />
-                                <motion.div
-                                    className={styles.frame}
-                                    initial={{
-                                        width: '10px',
-                                        height: '0px',
-                                        color: "black",
-                                        left: '80%'
-                                    }}
-                                    animate={{
-                                        height: ['0px', '5px', '10px'],
-                                        width: ['0px', '5px', '10px'],
-                                    }}
-                                    transition={{
-                                        duration: frameAppearDuration,
-                                        times: [0.1, 0.3, 0.5],
-                                        ease: "easeInOut"
-                                    }}
-                                />
-
-                            </motion.div>
-                        </div>
-                    </motion.div>
-                    <div className={styles.numbers}>
-                        <Numbers frameAppearDuration={frameAppearDuration - 1}/>
-                    </div>
-                    <motion.div
-                        className={styles.blueFlame}
-                        initial={{ opacity: 0}}
-                        animate={{ opacity: 1}}
-                        transition={{duration: 8, ease: "easeInOut", delay: frameAppearDuration - 1}}
-                    >
-                        <BlueFlame isSecondStage={isSecondStage}/>
-                    </motion.div>
+                <motion.div
+                    // initial={{top: '0', left: '0', translate: "(-50%, -50%)"}}
+                    // animate={{
+                    //     rotateY: "0.5turn",
+                    // }}
+                    // transition={{
+                    //     duration: 10,
+                    //     ease: "backOut", when: "afterChildren", delay: 7
+                    // }}
+                    className={styles.centerContent}>
+                    <Frames frameAppearDuration={frameAppearDuration}/>
+                    <Numbers frameAppearDuration={frameAppearDuration - 1}/>
+                    <BlueFlame frameAppearDuration={frameAppearDuration} isSecondStage={isSecondStage}/>
                     <Chain frameAppearDuration={frameAppearDuration}/>
-                </div>
+                </motion.div>
             </div>
         </>
     );
--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,4 +1,4 @@
-import {AnimatePresence, motion} from "framer-motion";
+import {motion} from "framer-motion";
 import {useEffect, useState} from "react";
 import styles from "./Numbers.module.scss"
 import {IColoredNumber, ISetNumbersPayload} from "../interfaces/NumberInterfaces";
@@ -73,29 +73,32 @@ export default function Numbers({frameAppearDuration}: { frameAppearDuration: nu
     }, []);
 
 
-    return (<>
-        <AnimatePresence>
-            {numberState.map((items: IColoredNumber[], index: number) => (
-                <motion.div
-                    className={styles.numberRow}
-                    key={`${index}`}
-                    initial={{width: '0px', opacity: 0}}
-                    animate={{width: '100%', opacity: 1}}
-                    transition={{duration: 1, ease: "easeOut", delay: frameAppearDuration}}
-                >{items.map((number: IColoredNumber, index2: number) => {
-                    indexOfNumber++;
-                    return (
-                        <motion.div
-                            className={styles.number}
-                            key={`${index}-${index2}`}
-                            initial={{color: 'white', opacity: 0}}
-                            animate={{color: number.isWhite ? "white" : "green", opacity: 1}}
-                            transition={{duration: 1, ease: "easeOut", delay: indexOfNumber * STEP_DURATION + frameAppearDuration}}
-                        >{number.digit ? 1 : 0}</motion.div>
-                    )
-                })}
-                </motion.div>
-            ))}
-        </AnimatePresence>
-    </>)
+    return (
+        <div className={styles.numbersWrapper}>
+                {numberState.map((items: IColoredNumber[], index: number) => (
+                    <motion.div
+                        className={styles.numberRow}
+                        key={`${index}`}
+                        initial={{width: '0px', opacity: 0}}
+                        animate={{width: '100%', opacity: 1}}
+                        transition={{duration: 1, ease: "easeOut", delay: frameAppearDuration}}
+                    >{items.map((number: IColoredNumber, index2: number) => {
+                        indexOfNumber++;
+                        return (
+                            <motion.div
+                                className={styles.number}
+                                key={`${index}-${index2}`}
+                                initial={{color: 'white', opacity: 0}}
+                                animate={{color: number.isWhite ? "white" : "green", opacity: 1}}
+                                transition={{
+                                    duration: 1,
+                                    ease: "easeOut",
+                                    delay: indexOfNumber * STEP_DURATION + frameAppearDuration
+                                }}
+                            >{number.digit ? 1 : 0}</motion.div>
+                        )
+                    })}
+                    </motion.div>
+                ))}
+        </div>)
 }
